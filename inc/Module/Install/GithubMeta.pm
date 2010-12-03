ceb4c50998b36f223ec9cb754df55288286a0367
Chris Williams <chris@bingosnet.co.uk>
2010-12-03 10:24:54 +0000

    Import of BINGOS/Task-Module-Install-BINGOS-1.02 from CPAN.
    gitpan-cpan-distribution: Task-Module-Install-BINGOS
    gitpan-cpan-version:      1.02
    gitpan-cpan-path:         BINGOS/Task-Module-Install-BINGOS-1.02.tar.gz
    gitpan-cpan-author:       BINGOS
    gitpan-cpan-maturity:     released

--- inc/Module/Install/GithubMeta.pm
+++ inc/Module/Install/GithubMeta.pm
@@ -3,17 +3,18 @@ package Module::Install::GithubMeta;
 
 use strict;
 use warnings;
+use Cwd;
 use base qw(Module::Install::Base);
 use vars qw($VERSION);
 
-$VERSION = '0.02';
+$VERSION = '0.10';
 
 sub githubmeta {
   my $self = shift;
   return unless $Module::Install::AUTHOR;
-  return unless -e '.git';
+  return unless _under_git();
   return unless $self->can_run('git');
-  return unless my ($git_url) = `git remote show origin` =~ /URL: (.*)$/m;
+  return unless my ($git_url) = `git remote show -n origin` =~ /URL: (.*)$/m;
   return unless $git_url =~ /github\.com/; # Not a Github repository
   my $http_url = $git_url;
   $git_url =~ s![\w\-]+\@([^:]+):!git://$1/!;
@@ -24,7 +25,26 @@ sub githubmeta {
   return 1;
 }
 
+sub _under_git {
+  return 1 if -e '.git';
+  my $cwd = getcwd;
+  my $last = $cwd;
+  my $found = 0;
+  while (1) {
+    chdir '..' or last;
+    my $current = getcwd;
+    last if $last eq $current;
+    $last = $current;
+    if ( -e '.git' ) {
+       $found = 1;
+       last;
+    }
+  }
+  chdir $cwd;
+  return $found;
+}
+
 'Github';
 __END__
 
-#line 88
+#line 108
